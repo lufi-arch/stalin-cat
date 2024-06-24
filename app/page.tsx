6bb61dba6910c52e70c20bfbfd5d61a9fb71ac11
FathZulfaAli <fathzulfa34@gmail.com>
2024-06-24 14:43:11 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import hero from "@/public/hero.png";
+import stand from "@/public/stand.png";
+import RollingText from "@/components/RollingText";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full bg-black">
+      <RollingText />
+      <Image src={hero} alt="CAAATTT" />
+
+      <div className="flex flex-row text-white gap-x-32">
+        <Link href={"https://x.com/StalinsCatOnSol"} target="blank">
+          <div className="items-center justify-center flex flex-col">
+            <div className="bg-white h-20 w-20 rounded-full shadow-lg shadow-red-700 overflow-hidden items-center justify-center flex">
+              <Image
+                src={
+                  "https://upload.wikimedia.org/wikipedia/commons/c/ce/X_logo_2023.svg"
+                }
+                alt="x logo"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>Twitter</span>
+          </div>
+        </Link>
+
+        <Link href={"https://t.me/StalinCatLover"} target="blank">
+          <div className="items-center justify-center flex flex-col">
+            <div className="bg-white h-20 w-20 rounded-full shadow-lg shadow-red-700 overflow-hidden items-center justify-center flex">
+              <Image
+                src={
+                  "https://upload.wikimedia.org/wikipedia/commons/e/ef/Telegram_X_2019_Logo.svg"
+                }
+                alt="x logo"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>Telegram</span>
+          </div>
+        </Link>
+
+        <div className="items-center justify-center flex flex-col">
+          <div className="bg-white h-20 w-32 rounded-full shadow-lg shadow-red-700 items-center justify-center flex">
+            <span className="text-black font-extrabold text-2xl">Pumpfun</span>
+          </div>
+          <span>Pumpfun</span>
+        </div>
+      </div>
+      <div className="flex justify-center w-full bg-white">
+        <span className="text-[60px] font-extrabold text-red-800">
+          Staliens Cat Really Likes You Comerades
+        </span>
+      </div>
+      <Image src={stand} alt="war" />
+    </div>
+  );
+}
---
+++ components/RollingText.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const RollingText: React.FC = () => {
+  return (
+    <div className="overflow-hidden whitespace-nowrap max-w-full fixed top-0 bg-white">
+      <div className="flex-col inline-block animate-marquee">
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Join the Mission with Stalin&apos;s Cat Solana Token!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Help Us Reach 50 Members and Launch the Revolution!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Invite Friends and Be a Part of the Future!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Every Invite Counts - Make Papa Proud!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Join the Mission with Stalin&apos;s Cat Solana Token!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Help Us Reach 50 Members and Launch the Revolution!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Invite Friends and Be a Part of the Future!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Every Invite Counts - Make Papa Proud!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Join the Mission with Stalin&apos;s Cat Solana Token!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Help Us Reach 50 Members and Launch the Revolution!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Invite Friends and Be a Part of the Future!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Every Invite Counts - Make Papa Proud!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Join the Mission with Stalin&apos;s Cat Solana Token!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Help Us Reach 50 Members and Launch the Revolution!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Invite Friends and Be a Part of the Future!
+        </span>
+        <span className="px-4 font-extrabold text-[50px] bg-white text-red-800">
+          | Every Invite Counts - Make Papa Proud!
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default RollingText;
